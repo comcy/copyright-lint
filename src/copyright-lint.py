20e4de2f5acad7b0cb30826f10add177acde92a1
Christian Silfang <christian.silfang@gmail.com>
2023-07-17 23:12:16 +0200

    Added initial copyright-lint script, Readme and License

---
+++ src/copyright-lint.py
@@ -0,0 +1,35 @@
+import re
+import glob
+import fnmatch
+
+CPY = """@copyright Copyright"""
+path = "./**/*.ts"
+
+findings = []
+
+def printPositive(file):
+    print('[X]', file)
+
+def printNegative(file):
+    print('[ ]', file)
+
+def addToNegativeList(file):
+    findings.append(file)
+
+for path in glob.glob(path, recursive=True):
+
+    with open(path) as f:
+
+        filtered = fnmatch.filter(f, CPY)
+        print(filtered)
+
+        # if CPY in f.read():
+        #     printPositive(path)
+        # else:
+        #     printNegative(path)
+        #     addToNegativeList(path)
+
+print("----------------------------------------------------")
+print("Please check following files: \n")
+for finding in findings:
+    print("\t", finding)
