fffbb7acff66b0adaa088e64cfc84aff358a6de0
Christian Silfang <christian.silfang@gmail.com>
2023-10-03 20:39:34 +0200

    Updated rollup config

--- nodejs/buildtools/cleanup-package.js
+++ nodejs/buildtools/cleanup-package.js
@@ -1,24 +1,22 @@
-const fs = require("fs"); // read lib name from cmd param
+import * as fs from 'fs';
 
 const workingDirectory = process.env.PWD;
 
-const PKG_ORIGIN_PATH = `${workingDirectory}/package.json`;
-const PKG_CACHED_PATH = `${workingDirectory}/package-cached.json`;
+const PKG_DIST_PATH = `${workingDirectory}/dist/package.json`;
+// const PKG_CACHED_PATH = `${workingDirectory}/package-cached.json`;
 
-console.log("cleanup ...");
-
-// Create a cached version of package.json
-fs.copyFile(PKG_ORIGIN_PATH, PKG_CACHED_PATH, (err) => {
-  if (err) {
-    throw err;
-  }
-});
+console.info('Start cleanup `package.json` ...');
 
 // Cleanup sections in package.json
-const packageJson = JSON.parse(fs.readFileSync(PKG_ORIGIN_PATH).toString());
+const packageJson = JSON.parse(fs.readFileSync(PKG_DIST_PATH).toString());
 
 delete packageJson.devDependencies;
+console.info('... devDependencies');
 delete packageJson.dependencies;
+console.info('... dependencies');
 delete packageJson.scripts;
+console.info('... scripts');
 
-fs.writeFileSync(PKG_ORIGIN_PATH, JSON.stringify(packageJson, null, 2));
+fs.writeFileSync(PKG_DIST_PATH, JSON.stringify(packageJson, null, 2));
+
+console.info('Finished cleanup `package.json`');
--- nodejs/rollup.config.js
+++ nodejs/rollup.config.js
@@ -1,37 +1,63 @@
 import typescript from 'rollup-plugin-typescript2';
-import banner from 'rollup-plugin-banner';
+import { terser } from 'rollup-plugin-terser';
+import del from 'rollup-plugin-delete';
+import copy from 'rollup-plugin-copy';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json' assert { type: 'json' };
 
-
-const BANNER = `
-  @license
-	xxx - xxx.js - ${pkg.version}
-	Released under the ISC License!.
-`;
+const BANNER = '#!/usr/bin/env node';
 
 export default {
     input: 'src/index.ts', // Entry point for your library
     output: [
         {
-            file: 'dist/index.cjs.js', // CommonJS format
-            format: 'cjs',
-            sourcemap: true
-        },
-        {
-            file: 'dist/index.esm.js', // ES module format
-            format: 'es',
-            sourcemap: true
-        }
-    ],
+            file: pkg.main, // ES module format
+            format: 'esm',
+            sourcemap: true,
+            plugins: [
+                terser({
+                    ecma: 2020,
+                    mangle: { toplevel: true },
+                    compress: {
+                        module: true,
+                        toplevel: true,
+                        unsafe_arrows: true,
+                        drop_console: true,
+                        drop_debugger: true,
+                    },
+                    output: { quote_style: 1 },
+                }),
+            ],
+            banner: BANNER,
 
+        },
+    ],
     external: [
         ...Object.keys(pkg.scripts || {}),
         ...Object.keys(pkg.dependencies || {}),
         ...Object.keys(pkg.peerDependencies || {}),
-        'fs'
     ],
     plugins: [
+        peerDepsExternal(), // Exclude peer dependencies from the bundle
         typescript(), // Compile TypeScript files
+        nodeResolve(), // Resolve node_modules
+        copy({
+            targets: [
+                {
+                    src: [
+                        '../LICENSE',
+                        '../README.md',
+                        './package.json'
+                    ],
+                    dest: 'dist'
+                },
+            ],
+            hook: 'writeBundle',
+            copyOnce: true,
+            copySync: true,
+        }),
+        del({ targets: ['dist'] }), // Clean the output directory
     ],
     external: [], // Specify external dependencies here
 };
